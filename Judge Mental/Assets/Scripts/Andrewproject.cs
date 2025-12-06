7ca901b879c71a2c2231e96a626575074f2dd42a
Starlitriver1234 <C24745999@mytudublin.ie>
2025-12-06 16:40:18 +0000

    Fixed the main menu. Now the game starts the moment you press start.

--- Judge Mental/Assets/Scripts/Andrewproject.cs
+++ Judge Mental/Assets/Scripts/Andrewproject.cs
@@ -7,11 +7,18 @@ public class Andrewprojects : MonoBehaviour
 {
 public NPCConversation myConversation;
 
+/*
 private void OnMouseOver()
 {
     if (Input.GetMouseButtonDown(0))
     {
         ConversationManager.Instance.StartConversation(myConversation);
     }
-}
+} */
+
+private void Start()
+    {
+        if (ConversationManager.Instance != null)
+            ConversationManager.Instance.StartConversation(myConversation);
+    }
 }
